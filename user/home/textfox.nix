44ffe210b8abf071f849eb37233692106fcdea41
Garret R. <norsemangef@protonmail.com>
2025-06-15 17:23:54 -0600

    fixed a typo

--- user/home/textfox.nix
+++ user/home/textfox.nix
@@ -4,7 +4,7 @@
   imports = [ inputs.textfox.homeManagerModules.default ];
 
   firefox.enable = true;
-  firefox.extensions = with inputs.firefox-addons.pkgs.${pkgs.system}; [ ublock-origin ];
+  firefox.extensions = with inputs.firefox-addons.packages.${pkgs.system}; [ ublock-origin ];
 
   textfox = {
     enable = true;
