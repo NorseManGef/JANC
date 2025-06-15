36041ce4c89928183510ab5bc681a9ace5b5bd7a
Garret R. <norsemangef@protonmail.com>
2025-06-15 17:43:50 -0600

    firefox extensions added

--- user/home/textfox.nix
+++ user/home/textfox.nix
@@ -4,14 +4,19 @@
   imports = [ inputs.textfox.homeManagerModules.default ];
 
   programs.firefox.enable = true;
-  programs.firefox.profiles.textfox.extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [ ublock-origin ];
+  programs.firefox.profiles.textfox.extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [ 
+    ublock-origin
+    #sidebery
+    sponsorblock
+    multi-account-containers
+    firefox-color
+  ];
 
   textfox = {
     enable = true;
     profile = "textfox";
     config = {
       background.color = "#1d2021";
-      displayHorizontalTabs = true;
     };
   };
 }
