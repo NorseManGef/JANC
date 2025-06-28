5eec6bc0ee0f4f725015550812e0e5c6752774f2
Garrett R. <norsemangef@protonmail.com>
2025-06-28 16:52:32 -0600

    turn tab length back to 2

--- system/nvf-configuration.nix
+++ system/nvf-configuration.nix
@@ -11,8 +11,8 @@
       };
 
       options = {
-        shiftwidth = 4;
-        tabstop = 4;
+        shiftwidth = 2;
+        tabstop = 2;
         autoindent = true;
         mouse = "";
       };
