58ea7394514f1a9a3c44b821064e77922ff1a9a2
Garrett R. <norsemangef@protonmail.com>
2025-06-29 12:16:35 -0600

    more nvim tweaks

--- system/nvf-configuration.nix
+++ system/nvf-configuration.nix
@@ -92,7 +92,7 @@
             "cpp"
           ]; 
           callback = lib.mkLuaInline '' 
-            function() vim.bo.tabstop = 4 end 
+            function() vim.bo.tabstop = 4 vim.bo.shiftwidth = 4 end
           ''; 
         } 
       ];
