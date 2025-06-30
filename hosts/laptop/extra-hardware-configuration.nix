16518ae568f64728d9faa153ad075daa79d2a839
Garrett R. <norsemangef@protonmail.com>
2025-06-30 17:59:47 -0600

    swap nvidia offload to sync

--- hosts/laptop/extra-hardware-configuration.nix
+++ hosts/laptop/extra-hardware-configuration.nix
@@ -2,13 +2,13 @@
 
 {
   hardware.nvidia.open = true;
+  hardware.nvidia.modesetting = true;
 
   hardware.nvidia.prime = {
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
 
-    offload.enable = true;
-    offload.enableOffloadCmd = true;
+    sync.enable = true; 
   };
   
   environment.systemPackages = [ pkgs.nvtopPackages.full pkgs.brightnessctl ];
