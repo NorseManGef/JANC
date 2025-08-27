4dea74f14d747c549d3f688d40af2d7466bba59f
Garret R. <norsemangef@protonmail.com>
2025-08-27 13:02:26 -0600

    finally got disablePimax.service working

--- hosts/desktop/systemd.nix
+++ hosts/desktop/systemd.nix
@@ -6,8 +6,9 @@
     description = "disable pimax";
     serviceConfig.Type = "oneshot";
     script = ''
+      sleep 2
       echo '1-3.2.7' | tee /sys/bus/usb/drivers/usb/unbind
     '';
-    wantedBy = [ "graphical.target" ];
+    wantedBy = [ "graphical.target" "multi-user.target" "default.target" ];
   };
 }
