314f42db538ade7b2be9ad6c2c69876e2bec3d95
Garret R. <norsemangef@protonmail.com>
2026-03-04 13:02:23 -0700

    changed hyprland start command

--- system/greetd.nix
+++ system/greetd.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd hyprland --asterisks";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland --asterisks";
         user = "norsemangef";
       };
     };
