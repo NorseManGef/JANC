dc95fb70f366295e737d97c687639708d2f8b3f2
Garrett R. <norsemangef@protonmail.com>
2026-06-29 13:21:05 -0600

    removed disablePimax.service

--- hosts/desktop/systemd.nix
+++ hosts/desktop/systemd.nix
@@ -1,14 +1,14 @@
 { ... }:
 
 {
-  systemd.services.disablePimax = {
-    enable = true;
-    description = "disable pimax";
-    serviceConfig.Type = "oneshot";
-    script = ''
-      sleep 5
-      echo '1-3.4.7' | tee /sys/bus/usb/drivers/usb/unbind
-    '';
-    wantedBy = [ "graphical.target" "multi-user.target" "default.target" ];
-  };
+ #systemd.services.disablePimax = {
+ #  enable = true;
+ #  description = "disable pimax";
+ #  serviceConfig.Type = "oneshot";
+ #  script = ''
+ #    sleep 5
+ #    echo '1-3.4.7' | tee /sys/bus/usb/drivers/usb/unbind
+ #  '';
+ #  wantedBy = [ "graphical.target" "multi-user.target" "default.target" ];
+ #};
 }
